21d43488bb577874377815f01ad2d7d11dffa11a
Wikkens Peabody <davermn5@gmail.com>
2013-03-22 16:54:31 -0700

    beaverbranch : first commit

--- fourth_test.php
+++ fourth_test.php
@@ -1,4 +1,16 @@
 <?php
  //fourth_test.php
- echo 'fourth_test, hello wikkens';
+ echo 'fourth_test, hello wikkens </br></br>';
+ 
+ $data = array(
+           'name'    => "wikkens",
+           'hobbies' => "programming" 
+ );
+ 
+ 
+ echo 'The categories are: </br>';
+ foreach($data as $k1 => $v1){
+  echo $k1 . '</br>';
+ }
+ 
 ?>
